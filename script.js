dc6757151f500effb476feeb9e10e40f52a32cb1
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-06 08:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
+	  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+	// console.log(result);
+  return result;
 }
 
 // Do not change the code below.
